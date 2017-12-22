31e47672ab9ed66ab7a514cbda2afca8bf9f0139
smoarref <salar.moarref@gmail.com>
2017-12-21 22:29:18 -0500

    hello world python file

---
+++ test.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+print "hello world"
+print "second line"
+if True: 
+	print "true"
+	print "answer"
+else: 
+	print "false"
+	print "answer"
+
+raw_input("\n\n press any key to continue")  
+
+x=10 
+if x<20 :
+	print "x is small"
+
+
